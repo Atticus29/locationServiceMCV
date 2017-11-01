eca3345dc3b4754135a17cdf54f914de6fe943f4
Mark <mark.aaron.fisher@gmail.com>
2017-11-01 11:22:17 -0700

    continue to flesh out the location service

--- app/src/main/java/fisherdynamic/locationservicemcv/LocationService.java
+++ app/src/main/java/fisherdynamic/locationservicemcv/LocationService.java
@@ -1,13 +1,46 @@
 package fisherdynamic.locationservicemcv;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 public class LocationService extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Double currentLatitude;
+    private Double currentLongitude;
+    private Context serviceContext = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        serviceContext = this;
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
+        mGoogleApiClient.connect();
+    }
+
     public LocationService() {
     }
 
@@ -15,6 +48,7 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
     @Override
@@ -34,6 +68,49 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
 
     @Override
     public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            Log.d("personal", "location null");
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        } else {
+            Log.d("personal", "location not null");
+            handleNewLocation(location);
+        }
+    }
+
+    private void handleNewLocation(Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+        if (currentLatitude != null && currentLongitude != null) {
+            sendToActivity(currentLatitude, currentLongitude);
+        }
+    }
+
+    public void sendToActivity(Double currentLatitude, Double currentLongitude){
+        Intent intent = new Intent("locationServiceUpdates");
+        intent.putExtra("ServiceLatitudeUpdate", currentLatitude.toString());
+        intent.putExtra("ServiceLongitudeUpdate", currentLongitude.toString());
+        if(serviceContext != null){
+            LocalBroadcastManager.getInstance(serviceContext).sendBroadcast(intent);
+            Log.d("personal", "broadcast launched from the location service");
+            Toast.makeText(this, "broadcast launched from the location service", Toast.LENGTH_SHORT).show();
+        } else{
+            Log.d("personal", "didn't broadcast the location updates because serviceContext is null");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
